71552c19a1774c2a16cbb485d724993a40834679
ppanan33 <97427854+ppanan33@users.noreply.github.com>
2022-05-31 16:46:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
 
+let appInsights = require('applicationinsights');
+appInsights.setup("InstrumentationKey=6389b105-d371-4cef-856b-216325d4444c;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/").start();
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hello Node!\n'
